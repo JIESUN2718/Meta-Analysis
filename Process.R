5d5bf153b3ccc488bde12ed7bb3e0d12725761af
Jie_Sun <84234451+JIESUN2718@users.noreply.github.com>
2022-09-10 17:22:15 +1000

    new version

--- Process.R
+++ Process.R
@@ -1,36 +1,52 @@
-setwd("~/Desktop/Metadata/GBIF/2-南非大学标本馆")
+rm(list = ls())
 
 library(tidyverse)
 library(readr)
 library(maps )
 
-
+setwd("~/Desktop/k2208198/Meta-Analysis")
 # https://www.r4epi.com/importing-plain-text-files.html
-df <- read_delim ("verbatim.txt",delim  = "\t")
+df <- read_delim ("GBIF2.txt",delim  = "\t")
 
 
 dim(df)
 
-colnames(df)
+# colnames(df)
 
 # https://rstudio-pubs-static.s3.amazonaws.com/576093_5ea8c7b5464246cd989f082ae2bab9b0.html
 
-df$ID <- df$gbifID
+df <- df[,colSums(is.na(df))<nrow(df)]
 
-# geography with in 100 according to 29:16 video 2
-df$lon <- df$decimalLongitude
-df$lat <- df$decimalLatitude
-df$miniElevation <- df$minimumElevationInMeters
-df$maxiElevation <- df$maximumElevationInMeters
-#df$class NA
-#df$order <- df$order
-#df$family <- df$family
-# Do you need the value of "specificEpithet"
-table(df$specificEpithet)
+df <- df %>%
+  select(gbifID,individualCount,locality,verbatimElevation,decimalLatitude,decimalLongitude,
+         scientificName,
+         order,family,genus) %>%
+  filter(complete.cases(.))
+
+df %>% group_by(genus) %>%
+  select(genus)
+
+unique(df$scientificName)
+
+df %>%  group_by(genus) %>% select(genus,scientificName)
+
+# Scarabaeinae,Aphodiinae,Geotrupidae
+unique(df$genus)
+
+#count <- list('Onthophagus',)
+
+df[df$genus=='Kurtops']
+
+
+colnames(df)
+
+
+
+
 
 df$subfamily <- paste(df$genus ," ",df$specificEpithet)
 # subgenus is NA
-
+plot(df$decimalLongitude,df$decimalLatitude)
 
 
 
@@ -50,13 +66,13 @@ points(df$lon,
 
 
 
-max(df$lon)
-min(df$lon)
 
 
-df <- df %>%
-  select(ID,lon,lat,order,family) %>%
-  filter(complete.cases(.))
+
+
+
+
+plot(df$decimalLongitude,df$decimalLatitude)
 
 dim(df)
 
@@ -64,16 +80,14 @@ dim(df)
 
 str(df)
 
-df <- df[,colSums(is.na(df))<nrow(df)]
-str(df)
-
-
-
-sum(is.na(df$infraspecificEpithet))
 
 
 
 
+
+
+
+#https://stackoverflow.com/questions/69158910/keeping-2-digits-after-decimal-without-rounding-off
 
 
 
